3085623a28ef8fc26b3ad3a5b5f98ad77f641301
Asia Kane and Spencer Alan <asia@>
2017-07-05 13:28:34 -0700

    add authorization to allow only a signed in user to complete image create, update, and delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(user)
+    user_images_path(user)
+  end
+
+  def after_sign_out_path_for(users)
+    new_user_session_path
+  end
+
  protected
 
  def configure_permitted_parameters
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,8 @@
 class ImagesController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
-    @images = Image.all
+    user = User.find(params[:user_id])
+    @images = user.images.all
   end
 end
